de7d268d8565b1f69fed5d67f7c6844110ef2178
Prashanth <prashanth.polela@alumnux.com>
2023-02-07 14:58:11 +0530

    regex fils

--- regex.py
+++ regex.py
@@ -1,10 +1,33 @@
 import re
 def extract_string(input_string):
     mo = re.findall(r'\d+', input_string)
-    return mo[::2]
+    return ",".join(mo[::2])
 
 
 import re
 def characters_string(input_str):
     return ''.join(re.findall(r'[a-z]', input_str))
+
+
+# import re
+# string = "hello 12 hi 89.howdy 34"
+# mo = "" .join(re.findall('\d+', string))
+# print(mo)
+
+
+# import re
+# string = '39801 356, 2102 1111'
+# pattern = '(\d{3}) (\d{3})'
+# match = re.search(pattern, string)
+# if match:
+#     print(match.group())
+# else:
+#     print("pattern not found")
+
+
+import re
+def match_mac(input_string):
+    mo = re.findall(r'[2-9]\w+', input_string)
+    return ".".join(mo[2:-1])
+
 
--- regex.robot
+++ regex.robot
@@ -2,12 +2,19 @@
 Documentation    extract elements from a string
 Library    regex.py
 
-
+*** Variables ***
+${given_String}     12/ofd/20-1/scq/80  CM 203d.66ae.5c79 (Arris) D3.1 State=Operational D1.1/atdma PrimSID=8195 FiberNode= FN11
 *** Test Cases ***
 filter characters in a string
         ${output}=      regex.characters string     6477.7d90.6159
         log    ${output}
 
-find numbers in a string
+
+extract particular numbers in a string
         ${result}=      regex.extract string    600.000-792.000
         log    ${result}
+
+
+find the MAC address in a string
+        ${Mac_address}     regex.match mac      ${given_String}
+        log     ${Mac_address}
